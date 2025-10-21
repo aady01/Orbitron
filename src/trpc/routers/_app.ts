f6222c9c323dd914f659fe6db0dcec05532dc2b7
aady01 <aditya.aps000@gmail.com>
2025-10-21 17:01:03 +0530

    inngest setup / background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,21 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
   getUsers: protectedProcedure.query(async ({ ctx }) => {
-    console.log({ userId: ctx.auth.user.id });
-    return prisma.user.findMany({
-      where: { id: ctx.auth.user.id },
+    return prisma.user.findMany();
+  }),
+  getWorkflow: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "aady@gmail.com",
+      },
     });
+    return { sucess: true, message: "Job Queued" };
   }),
 });
 // export type definition of API

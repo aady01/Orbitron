b455772e051124b83bd4960007e9fbd0de62a332
aady01 <aditya.aps000@gmail.com>
2026-01-16 21:14:26 +0530

    Fixed the prisma migrations, developed the workflow editor space

---
+++ src/components/initial-nodes.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import type { NodeProps } from "@xyflow/react"
+import { PlusIcon } from "lucide-react"
+import { memo } from "react"
+import { PlaceholderNode } from "./react-flow/placeholder-node"
+import { WorkflowNode } from "./workflow-node"
+
+export const InitialNode = memo((props: NodeProps) => {
+    return (
+        <WorkflowNode>
+            <PlaceholderNode
+                {...props}
+            >
+                <div className="flex items-center justify-center cursor-pointer">
+                    <PlusIcon className="size-4" />
+                </div>
+
+            </PlaceholderNode>
+        </WorkflowNode>
+    )
+})
